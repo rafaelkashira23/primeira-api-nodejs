60fc7d3331e52b563e54e4f8bbd006f11096f0f8
rafaelkashira23 <rafaelkashira@gmail.com>
2025-01-02 11:06:13 -0300

    http

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const http = require('http');
-const servidor = http.createServer();
+const servidor = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain', charset='utf-8');
+    res.end('Olá, TIC em trilhas.\n');
+});
+const porta = 3000;
+const host = 'localhost';
+servidor.listen(porta, host, () => {
+    console.log(`Servidor executando em http://${host}:${porta}/`);
+});
