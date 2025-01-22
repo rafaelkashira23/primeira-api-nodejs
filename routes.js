a8f7af28a4c32e6edb61cc7883cd9373bc08509f
rafaelkashira23 <rafaelkashira@gmail.com>
2025-01-22 02:06:05 -0300

    na função rotas, foi extraído o objeto conteudo

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 export default function rotas (req, res, dado){
+    const { conteudo } = dado;
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end(conteudo);
