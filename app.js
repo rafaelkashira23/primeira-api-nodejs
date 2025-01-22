53fa1f7611a77535d0bc9c18e894d4ac84a679d4
rafaelkashira23 <rafaelkashira@gmail.com>
2025-01-22 00:50:19 -0300

    criação de servidor http

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const servidor = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain', charset='utf-8');
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('Olá, TIC em trilhas.\n');
 });
 const porta = 3000;
