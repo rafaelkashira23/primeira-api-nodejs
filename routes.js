62143c6f8dc860819c90812c837b6d3258488556
rafaelkashira23 <rafaelkashira@gmail.com>
2025-01-22 06:30:33 -0300

    nh

--- routes.js
+++ routes.js
@@ -17,18 +17,19 @@ export default function rotas(req, res, dado) {
             corpo.push(parte);
         });
         req.on('end', () => {
-            const arquivo = JSON.parse(corpo);
+            const dados = Buffer.concat(corpo).toString();
+            const arquivo = JSON.parse(dados);
             if (!arquivo?.nome) {
                 const resposta = {
                     erro: {
                         mensagem: 'Arquivo não foi encontrado, porém é obrigatório'
                     }
-                }
-                JSON.stringify(resposta);
+                };
+                res.statusCode = 400;
+                res.end(JSON.stringify(resposta));
                 return;
             }
             fs.writeFile(`${arquivo.nome}.txt`, arquivo?.conteudo ?? '', 'utf-8', (erro) => {
-
                 if (erro) {
                     console.log('Falha ao escrever o arquivo', erro);
                     res.statusCode = 500;
@@ -40,10 +41,27 @@ export default function rotas(req, res, dado) {
                     res.end(JSON.stringify(resposta));
                     return;
                 }
-            })
-        }
-        
-    };
+                res.statusCode = 200;
+                const resposta = {
+                    mensagem: 'Arquivo criado com sucesso'
+                };
+                res.end(JSON.stringify(resposta));
+                return;
+            });
+            req.on('error', erro -> {
+                console.log('Falha ao processar a requisição', erro);
+                res.statusCode = 400;
+                const resposta = {
+                    erro: {
+                        mensagem: 'Falha ao processar a requisição'
+                    }
+                };
+                res.end(JSON.stringify(resposta));
+                return;
+            });
+            return;
+        });
+    }
     res.statusCode = 404; // Define explicitamente o status de recurso não encontrado
     const resposta = {
         mensagem: 'Recurso não encontrado',
