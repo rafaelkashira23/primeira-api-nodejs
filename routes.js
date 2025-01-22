0b719cc3b48c02eab731f27ef4e889849cb64ae6
rafaelkashira23 <rafaelkashira@gmail.com>
2025-01-22 04:53:00 -0300

    sdfsd

--- routes.js
+++ routes.js
@@ -1,13 +1,21 @@
+import { url } from "inspector";
+
 export default function rotas (req, res, dado){
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    if(req.method === 'GET' req.url === '/'){
+    if(req.method === 'GET' && req.url === '/'){
         const { conteudo } = dado;
         res.statusCode = 200;
         const resposta = {
             mensagem: conteudo
         };
-        res.end(resposta);
+        res.end(JSON.stringify(resposta);
     return;
     }
+    res.statusCode = 404;
+    const resposta = {
+        mensagem: 'Recurso não encontrado'
+        url: req.url
+    };
+    res.end(JSON.stringify(resposta));
 
 }
