199d1cb864dd28f4b553c2ef20e77a45e495ee55
rafaelkashira23 <rafaelkashira@gmail.com>
2025-01-22 01:06:42 -0300

    criação do arquivo de texto mensagem.txt modulo fs

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const http = require('http');
+//importação do módulo http e do módulo fs
+import http from 'http';
+import fs, { writeFile } from 'fs';
+
+//criação de arquivo de texto
+fs.writeFile('mensagem.txt', 'Olá, TIC em trilhas do arquivo!', 'utf-8', (erro)=>{
+    if(erro){
+        console.log('Falha ao escrever o arquivo', erro);
+    }
+    console.log('Arquivo criado com sucesso!');
+}
+
+//criação do servidor
 const servidor = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
