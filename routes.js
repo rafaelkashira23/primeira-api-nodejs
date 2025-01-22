ba44b7f07e3b9bf124b913c11450e4bd97aa3c14
rafaelkashira23 <rafaelkashira@gmail.com>
2025-01-22 05:11:40 -0300

    adsaf

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 import { url } from "inspector";
 
-export default function rotas (req, res, dado){
+export default function rotas(req, res, dado) {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    if(req.method === 'GET' && req.url === '/'){
+    if (req.method === 'GET' && req.url === '/') {
         const { conteudo } = dado;
         res.statusCode = 200;
         const resposta = {
@@ -11,28 +11,28 @@ export default function rotas (req, res, dado){
         res.end(JSON.stringify(resposta));
         return;
     }
-    }
-    if(req.method === 'POST' && req.url === '/arquivos'){
-        const corpo = [];
-        req.on('data', (parte) => {
-            corpo.push(parte);
-        });
-        req.on('end', () => {
-            const arquivo = JSON.parse(corpo);
-            if(!arquivo?.nome) {
-                const resposta = {
-                    erro: {
-                        mensagem: 'Arquivo não foi encontrado'
-                    }
+}
+if (req.method === 'POST' && req.url === '/arquivos') {
+    const corpo = [];
+    req.on('data', (parte) => {
+        corpo.push(parte);
+    });
+    req.on('end', () => {
+        const arquivo = JSON.parse(corpo);
+        if (!arquivo?.nome) {
+            const resposta = {
+                erro: {
+                    mensagem: 'Arquivo não foi encontrado'
                 }
             }
-        })
-    }
+        }
+    })
+}
 
-    res.statusCode = 404;
-    const resposta = {
-        mensagem: 'Recurso não encontrado',
-        url: req.url
-    };
-    res.end(JSON.stringify(resposta));
-    return;
+res.statusCode = 404;
+const resposta = {
+    mensagem: 'Recurso não encontrado',
+    url: req.url
+};
+res.end(JSON.stringify(resposta));
+return;
